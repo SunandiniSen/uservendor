5ff22d303e308736e4cdc731ef69181d86989ebd
Sunandini Sen <sunandini18@gmail.com>
2021-06-30 21:26:34 +0530

    init commit(2)

--- src/components/Createtable.js
+++ src/components/Createtable.js
@@ -4,7 +4,6 @@ import { useSelector } from 'react-redux';
 export const Createtable = (WrappedComponent, type) => {
     const data = useSelector(state => state[type][type]);
     const fields = useSelector(state => state[type].fields);
-    console.log('CreateTable',fields);
     return(
         <WrappedComponent data={data} fields={fields}/>
     )
